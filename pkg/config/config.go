854ef7a413d097a984b9dcb150c5e632b8d510ed
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:04:42 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -36,6 +36,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the Keycloak API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 )
 
